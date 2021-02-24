a024104a58eab014ba78d3c8f68e405b9fa75458
Jason Liebault <jason.liebault@simsoft-industry.fr>
2021-02-24 08:25:08 +0100

    Add CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('./middlewares/logger');
+var cors = require('cors')
 var config = require('./config');
 var db = require('./factories/mongoFactory');
 
@@ -14,6 +15,7 @@ const swaggerUi = require('swagger-ui-express');
 var app = express();
 
 app.use(logger.morgan);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
